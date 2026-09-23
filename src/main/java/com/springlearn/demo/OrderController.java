144dc7497aa3a92b5df16f302abb304684237c59
Sahilsthr <sahilsuthar.238@gmail.com>
2026-09-23 22:02:05 +0530

    Add REST GET endpoints

--- src/main/java/com/springlearn/demo/OrderController.java
+++ src/main/java/com/springlearn/demo/OrderController.java
@@ -1,17 +1,41 @@
 package com.springlearn.demo;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+
+
+
+@RestController
 public class OrderController {
 
     @GetMapping("/order")
-    @ResponseBody
+    
     public String order(){
         return "Order Placed Successfully!";
     }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello sahil!";
+    }
+    
+    @GetMapping("/learning_springboot")
+    public String learning_springboot() {
+        return "Learning Spring boot!";
+    }
+    @GetMapping("/greet")
+    public String greet(@RequestParam String name) {
+        return "Hello " + name + "!";
+    }
+    @GetMapping("/keseho")
+    public String kesoho(@RequestParam String how) {
+        return "hola amigo kese ho " + how +"!";
+    }
+    
+    
+    
     
 }
