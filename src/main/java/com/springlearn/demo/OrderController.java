9a2835c8d904356d66c640392b3b85063bb83075
Sahilsthr <sahilsuthar.238@gmail.com>
2026-09-24 09:59:34 +0530

    Add POST request body API

--- src/main/java/com/springlearn/demo/OrderController.java
+++ src/main/java/com/springlearn/demo/OrderController.java
@@ -2,6 +2,7 @@ package com.springlearn.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,10 @@ public class OrderController {
     @GetMapping("/keseho")
     public String kesoho(@RequestParam String how) {
         return "hola amigo kese ho " + how +"!";
+    }
+    @GetMapping ("/greet1/{name}")
+    public String greet1(@PathVariable String name){
+        return "hello " + name; 
     }
     
     
